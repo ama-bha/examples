c1bc3543ca1f65d8ac0ff00c6fb84ca12f1b2321
Amandeep Bharti <aman.bharti@hotmail.com>
2017-04-08 18:48:50 -0400

    fixed untracked files

---
+++ cpp/SmartPtrs/SmartPtrs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SmartPtrs
+//
+//  Created by Amandeep Bharti on 4/8/17.
+//  Copyright © 2017 Amandeep Bharti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
